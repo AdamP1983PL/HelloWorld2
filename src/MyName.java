6a8f45067b98249c151f08067e30c2f95fbb7c29
podlaski <podlaski83@gmail.com>
2022-10-09 13:02:59 +0200

    My name is...

---
+++ src/MyName.java
@@ -0,0 +1,8 @@
+import java.sql.SQLOutput;
+
+public class MyName {
+    public static void main(String[] args) {
+        String name = "Adam";
+        System.out.println("Mam na imię " + name);
+    }
+}

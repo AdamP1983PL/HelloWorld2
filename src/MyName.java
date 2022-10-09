3d81b6a4c9a9e5e90a42c8fa90450875900f5521
podlaski <podlaski83@gmail.com>
2022-10-09 13:05:04 +0200

    My name is...x3

--- src/MyName.java
+++ src/MyName.java
@@ -4,5 +4,7 @@ public class MyName {
     public static void main(String[] args) {
         String name = "Adam";
         System.out.println("Mam na imię " + name);
+        System.out.println("Mam na imię " + name);
+        System.out.println("Mam na imię " + name);
     }
 }
